c8e1f4837ffaeeacef08a5b74de00b9e725a8115
jackrichter <jackrichter1@gmail.com>
2023-07-17 22:46:16 +0200

    Js: Array methods filter and find

---
+++ jsFilterAndFindMethods.js
@@ -0,0 +1,4 @@
+console.log([7, 64, 6, -23, 11].filter((el) => el > 10));
+
+// Find returns the (value) first element found after filtering with a condition
+console.log([7, 64, 6, -23, 11].find((el) => el > 10));
