032a190905b0f9b4c193f30390397d38a75c7576
jackrichter <jackrichter1@gmail.com>
2023-07-18 13:43:17 +0200

    React: Application skeleton: basic components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import "./style.css";
 
+// Each function in JSX is a component. App is allways the first component.
 function App() {
+  const appTitle = "Today I Learned";
+
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share A Fact</button>
-    </header>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share A Fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactsList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>category filter</aside>;
+}
+
+function FactsList() {
+  return <section>facts list</section>;
 }
 
 export default App;
